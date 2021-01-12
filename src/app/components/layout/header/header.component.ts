d3961f2eebac519f012c72ea5e423a25031e33f0
Nick Luckey <38598401+keyluck@users.noreply.github.com>
2021-01-12 15:34:38 -0800

    Changed sidenavbar links to use ngFor

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavLinks } from './navlinks'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  sidenavlinks: NavLinks[] = [
+    { name: "Dashboard", url: "/", icon: "grid-chart" },
+    { name: "User Profile", url: "/userprofile", icon: "user"},
+    { name: "Table List", url: "/userprofile", icon: "tasks"},
+    { name: "Typography", url: "/userprofile", icon: "list"},
+    { name: "Icons", url: "/userprofile", icon: "nodes"},
+    { name: "Maps", url: "/userprofile", icon: "map-marker"},
+    { name: "Notifications", url: "/userprofile", icon: "bell"},
+    { name: "RTL Support", url: "/userprofile", icon: "network-globe"},
+  ]
+  
 
   constructor() { }
 
