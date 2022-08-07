1bcb6b816a33a6abd30db4a7a0862beeebda78f8
keyluck <nickluckey@gmail.com>
2022-08-07 10:06:33 -0700

    Update gitignore

--- src/app/components/dashboard/med-cards/line-chart/line-chart.component.ts
+++ src/app/components/dashboard/med-cards/line-chart/line-chart.component.ts
@@ -1,17 +1,22 @@
-import { Component, Input, SimpleChange} from '@angular/core';
-import { ChartType, ChartDataSets, ChartOptions, GridLineOptions} from 'chart.js';
+import { Component, Input, SimpleChange } from '@angular/core';
+import {
+  ChartType,
+  ChartDataSets,
+  ChartOptions,
+  GridLineOptions,
+} from 'chart.js';
 import { Color, Label } from 'ng2-charts';
 import { MedChartInfo } from '../medChartInfo';
 interface OnChanges {
-  ngOnChanges(changes: SimpleChange):void
+  ngOnChanges(changes: SimpleChange): void;
 }
 
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.scss']
+  styleUrls: ['./line-chart.component.scss'],
 })
-export class LineChartComponent implements OnChanges{
+export class LineChartComponent implements OnChanges {
   @Input() chartInfo!: MedChartInfo;
   lineChartData: ChartDataSets[] = [];
   lineChartLabels: Label[] = [];
@@ -21,21 +26,15 @@ export class LineChartComponent implements OnChanges{
         data: this.chartInfo.data,
       },
     ];
-    this.lineChartLabels = this.chartInfo.labels
-    
-    
-  
+    this.lineChartLabels = this.chartInfo.labels;
   }
-  
-
-  
 
   lineChartOptions: ChartOptions = {
     responsive: true,
-    
+
     scales: {
       scaleLabel: {
-        fontColor: 'white'
+        fontColor: 'white',
       },
       xAxes: [
         {
@@ -43,34 +42,27 @@ export class LineChartComponent implements OnChanges{
             color: 'white',
             zeroLineColor: 'white',
           },
-          
-        }
+        },
       ],
       yAxes: [
         {
           gridLines: {
             color: 'white',
             zeroLineColor: 'white',
-          }
-        }
-      ]
+          },
+        },
+      ],
     },
-    
-    
-    
-    
   };
 
   lineChartColors: Color[] = [
     {
       borderColor: 'white',
       pointBackgroundColor: 'white',
-      
     },
-  ]
-  
+  ];
+
   lineChartLegend = false;
   lineChartPlugins = [];
-  lineChartType:ChartType = `line`;
-  
+  lineChartType: ChartType = `line`;
 }
